cb6d00dc869a5b93068e75b194501b994a82b6c1
MuthuKumaran M <127849469+Muthumk123@users.noreply.github.com>
2025-12-26 19:54:01 +0530

    Add files via upload

--- Univariate.py
+++ Univariate.py
@@ -11,3 +11,16 @@ class Univariate():
                 #print("quan")
                 quan.append(columnName)
         return quan,qual
+class central_tendency_Univariate():
+    def mean_median_mode(dataset,quan):
+        import pandas as pd
+        descriptive=pd.DataFrame(index=["Mean","Median","Mode"],columns=quan)
+        for ColumnName in quan:
+            # descriptive[ColumnName]["Mean"]=dataset[ColumnName].mean()
+            # descriptive[ColumnName]["Median"]=dataset[ColumnName].median()
+            # descriptive[ColumnName]["Mode"]=dataset[ColumnName].mode()[0]
+             descriptive.loc ["Mean",ColumnName]=dataset[ColumnName].mean()
+             descriptive.loc ["Median",ColumnName]=dataset[ColumnName].median()
+             descriptive.loc ["Mode",ColumnName]=dataset[ColumnName].mode()[0]
+        return descriptive
+            
