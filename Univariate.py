dc9ad6ce3cf6c5e78788ec47195d82f738e2c5af
MuthuKumaran M <127849469+Muthumk123@users.noreply.github.com>
2025-12-27 08:22:51 +0530

    Add files via upload

--- Univariate.py
+++ Univariate.py
@@ -12,10 +12,10 @@ class Univariate():
                 quan.append(columnName)
         return quan,qual
 class central_tendency_percentile():
-    def mean_median_mode_percentile(dataset,quan):
+    def MMM_IQR_per(dataset,quan):
          import numpy as np
          import pandas as pd
-         descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%","Q4:100%"],columns=quan)
+         descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%",                                                                         "Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)                                  
          for ColumnName in quan:
               # descriptive[ColumnName]["Mean"]=dataset[ColumnName].mean()
               # descriptive[ColumnName]["Median"]=dataset[ColumnName].median()
@@ -28,6 +28,13 @@ class central_tendency_percentile():
                 descriptive.loc ["Q3:75%",ColumnName]=dataset.describe()[ColumnName]["75%"]
                 descriptive.loc ["99%",ColumnName]=np.percentile(dataset[ColumnName],99)
                 descriptive.loc ["Q4:100%",ColumnName]=dataset.describe()[ColumnName]["max"]
+                descriptive.loc ["IQR",ColumnName]=descriptive.loc ["Q3:75%",ColumnName]-descriptive.loc ["Q1:25%",ColumnName]
+                descriptive.loc ["1.5rule",ColumnName]=1.5* descriptive.loc ["IQR",ColumnName]
+                descriptive.loc ["Lesser",ColumnName]=descriptive.loc ["Q1:25%",ColumnName]-descriptive.loc ["1.5rule",ColumnName]
+                descriptive.loc ["Greater",ColumnName]=descriptive.loc ["Q3:75%",ColumnName]+descriptive.loc ["1.5rule",ColumnName]
+                descriptive.loc ["Min",ColumnName]=dataset[ColumnName].min()
+                descriptive.loc ["Max",ColumnName]=dataset[ColumnName].max()
+        
          return descriptive
                 
                     
