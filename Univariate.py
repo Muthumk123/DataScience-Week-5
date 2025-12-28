acc6bb248788e9c34ac2eff012a2b54f0912bf36
MuthuKumaran M <127849469+Muthumk123@users.noreply.github.com>
2025-12-28 15:34:09 +0530

    Add files via upload

--- Univariate.py
+++ Univariate.py
@@ -12,7 +12,7 @@ class Univariate():
                 quan.append(columnName)
         return quan,qual
 class central_tendency_percentile():
-    def MMM_IQR_per(dataset,quan):
+    def MMM_per_IQR(dataset,quan):
          import numpy as np
          import pandas as pd
          descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%",                                                                         "Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)                                  
@@ -34,14 +34,13 @@ class central_tendency_percentile():
                 descriptive.loc ["Greater",ColumnName]=descriptive.loc ["Q3:75%",ColumnName]+descriptive.loc ["1.5rule",ColumnName]
                 descriptive.loc ["Min",ColumnName]=dataset[ColumnName].min()
                 descriptive.loc ["Max",ColumnName]=dataset[ColumnName].max()
-        
+                descriptive.loc ["kurtosis",ColumnName]=dataset[ColumnName].kurtosis()
+                descriptive.loc ["skew",ColumnName]=dataset[ColumnName].skew()
          return descriptive
-
-class IQR():
-    def outlayer_column_names(MMM_IQR_per):
+    def outlayer_column_names(dataset,quan):
         from Univariate import Univariate
         from Univariate import central_tendency_percentile
-        #descriptive = MMM_IQR_per(dataset, quan)  # Automation after checking replaceing the value Lesser should not greater min
+        #descriptive = MMM_per_IQR(central_tendency_percentile)  # Automation after checking replaceing the value Lesser should not greater min
         lesser=[]
         greater=[]
         for ColumnName in quan:
@@ -50,16 +49,24 @@ class IQR():
             if(descriptive.loc ["Max", ColumnName] > descriptive.loc["Greater", ColumnName]):
                 greater.append(ColumnName)
         return lesser,greater
-    
-
-    def replace_in_the_outlayer():
-        lesser, greater = outlayer_column_names(dataset, quan)
+    def replace_in_the_outlayer(dataset,quan):
+        from Univariate import Univariate
+        from Univariate import central_tendency_percentile
+        #lesser, greater = outlayer_column_names(dataset, quan)
         for ColumnName in lesser:
             dataset[ColumnName] [dataset[ColumnName]<descriptive.loc["Lesser", ColumnName]]=descriptive.loc["Lesser", ColumnName]
         for ColumnName in greater:
             dataset[ColumnName] [dataset[ColumnName]>descriptive.loc["Greater", ColumnName]]=descriptive.loc["Greater", ColumnName]
         return  lesser, greater
      # descriptive = MMM_per_IQR(dataset, quan)  # after checking replaceing with  value to the dataset not to descriptive 
+    def freqTable(ColumnName,dataset):
+        import pandas as pd
+        freqTable=pd.DataFrame(columns=["Unique_Values","Frequencey","Releative_Frequencey","Cusum"])
+        freqTable["Unique_Values"]=dataset[ColumnName].value_counts().index
+        freqTable["Frequencey"]=dataset[ColumnName].value_counts().values
+        freqTable["Releative_Frequencey"]=(freqTable["Frequencey"]/103)
+        freqTable["Cusum"]=freqTable["Releative_Frequencey"].cumsum()
+        return freqTable
 
         
                     
